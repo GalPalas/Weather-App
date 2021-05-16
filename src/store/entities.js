43480e6ac3e06ff2a5841e6b6371c354747f2e35
Gal <galpalas89@gmail.com>
2021-05-16 13:08:14 +0300

    Add favorites card button

--- src/store/entities.js
+++ src/store/entities.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import weatherReducer from "../store/currentWeather/weather";
 import mainWeatherCardReducer from "../store/mainCard/mainWeatherCard";
+import favoritesCardReducer from "../store/favorites/card";
 
 export default combineReducers({
   weather: weatherReducer,
+  favoritesCards: favoritesCardReducer,
   weatherCard: mainWeatherCardReducer,
 });
